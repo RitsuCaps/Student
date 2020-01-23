948bc30f6c2a04f636bad8a1ec7dac78d7d85b97
RitsuCaps <dasmathe@sheridancollege.ca>
2020-01-22 23:12:20 -0500

    Ayaya

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -78,6 +78,20 @@ public class Student {
         this.email = email;
     }
 
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
 }
  
    
